33f0d614c4b08f0694ea3b63e0660cc609122eba
Spencer Zeng <spencerz@sfu.ca>
2020-04-23 16:38:46 -0400

    Add scaffolded code for Identity, update Startup.cs and _LoginPartial.cshtml to work with new data context

---
+++ Trackily/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Trackily.Areas.Identity.Data;
+using Trackily.Data;
+
+[assembly: HostingStartup(typeof(Trackily.Areas.Identity.IdentityHostingStartup))]
+namespace Trackily.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TrackilyContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TrackilyContextConnection")));
+
+                services.AddDefaultIdentity<TrackilyUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TrackilyContext>();
+            });
+        }
+    }
+}
