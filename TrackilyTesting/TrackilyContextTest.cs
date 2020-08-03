94592322e3db9a62fe7a7563680a12d7cba6c16d
Spencer Zeng <spencer_zeng@sfu.ca>
2020-08-02 20:01:37 -0700

    Add UserTicketService tests

--- TrackilyTesting/TrackilyContextTest.cs
+++ TrackilyTesting/TrackilyContextTest.cs
@@ -15,13 +15,6 @@ namespace TrackilyTesting
         [Fact]
         public void DeletingProject_DeletesAssociatedTickets()
         {
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
-
-            var options = new DbContextOptionsBuilder<TrackilyContext>()
-                .UseSqlite(connection)
-                .Options;
-
             var projectId = Guid.NewGuid();
             var ticketId = Guid.NewGuid();
 
@@ -36,13 +29,12 @@ namespace TrackilyTesting
                 TicketId = ticketId,
             });
 
+            var options = GetInMemoryDbContextOptions();
 
             using (var context = new TrackilyContext(options))
             {
                 context.Database.EnsureCreated();
-
                 context.Projects.Add(project);
-
                 context.SaveChanges();
             }
 
@@ -71,7 +63,83 @@ namespace TrackilyTesting
         [Fact]
         public void DeletingTicket_DeletesUserTicket()
         {
-            // TODO
+            var ticketId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+
+            var options = GetInMemoryDbContextOptions();
+
+            using (var context = new TrackilyContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                var user = new TrackilyUser()
+                {
+                    Id = userId,
+                    CreatedTickets = new List<Ticket>(),
+                    AssignedTo = new List<UserTicket>()
+                };
+
+                context.Users.Add(user); // User must exist in the database before creating a Ticket/UserTicket due to FK constraint.
+                context.SaveChanges();
+            }
+
+            using (var context = new TrackilyContext(options))
+            {
+                var userSaved = context.Users.Find(userId);
+                Assert.NotNull(userSaved);
+
+                var ticket = new Ticket()
+                {
+                    Project = new Project(),
+                    Creator = userSaved,
+                    TicketId = ticketId,
+                    Assigned = new List<UserTicket>()
+                };
+
+                var userTicket = new UserTicket()
+                {
+                    Id = userId,
+                    TicketId = ticketId,
+                    Ticket = ticket,
+                    User = userSaved
+                };
+
+                ticket.Assigned.Add(userTicket);
+
+                context.Tickets.Add(ticket);
+                context.SaveChanges();
+            }
+
+            using (var context = new TrackilyContext(options))
+            {
+                var ticketSaved = context.Tickets.Find(ticketId);
+                Assert.NotNull(ticketSaved);
+
+                var userTicketSaved = context.UserTickets.Find(userId, ticketId);
+                Assert.NotNull(userTicketSaved);
+
+                context.Tickets.Remove(ticketSaved);
+                context.SaveChanges();
+            }
+
+            using (var context = new TrackilyContext(options))
+            {
+                var tickets = context.Tickets.ToList();
+                Assert.Empty(tickets);
+
+                var userTickets = context.UserTickets.ToList();
+                Assert.Empty(userTickets);
+            }
+        }
+
+        public static DbContextOptions<TrackilyContext> GetInMemoryDbContextOptions()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            return new DbContextOptionsBuilder<TrackilyContext>()
+                .UseSqlite(connection)
+                .Options;
         }
     }
 }
---
+++ TrackilyTesting/UserTicketServiceTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Trackily.Areas.Identity.Data;
+using Trackily.Models.Domain;
+using Trackily.Services;
+using Xunit;
+
+namespace TrackilyTesting
+{
+    public class UserTicketServiceTest
+    {
+        private const string MockUsername = "USERNAME";
+
+        [Fact]
+        public void UserTicketToNames_ReturnsNamesCorrectly()
+        {
+            var context = GetDb();
+
+            var userId = Guid.NewGuid();
+            var ticketId = Guid.NewGuid();
+            SetupUserAndTicket(userId, ticketId, context);
+
+            var user = context.Users.Find(userId);
+            var ticket = context.Tickets.Find(ticketId);
+            ticket.Assigned.Add(new UserTicket
+            {
+                Id = userId,
+                TicketId = ticketId,
+                Ticket = ticket,
+                User = user
+            });
+            context.SaveChanges();
+
+            var ticketSaved = context.Tickets.Include(t => t.Assigned)
+                                            .Single(t => t.TicketId == ticketId);
+            var service = new UserTicketService(context);
+            var usernames = service.UserTicketToNames(ticketSaved.Assigned);
+
+            Assert.Single(usernames, MockUsername);
+        }
+
+        [Fact]
+        public void UserTicketToNames_NoAssignedUsersReturnsEmpty()
+        {
+            var context = GetDb();
+
+            var userId = Guid.NewGuid();
+            var ticketId = Guid.NewGuid();
+            SetupUserAndTicket(userId, ticketId, context);
+
+            var ticketSaved = context.Tickets.Include(t => t.Assigned)
+                .Single(t => t.TicketId == ticketId);
+            var service = new UserTicketService(context);
+            var usernames = service.UserTicketToNames(ticketSaved.Assigned);
+
+            Assert.Empty(usernames);
+        }
+
+        private TrackilyContext GetDb()
+        {
+            var options = TrackilyContextTest.GetInMemoryDbContextOptions();
+            var context = new TrackilyContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        private void SetupUserAndTicket(Guid userId, Guid ticketId, TrackilyContext context)
+        {
+            var user = new TrackilyUser()
+            {
+                Id = userId,
+                UserName = MockUsername,
+                CreatedTickets = new List<Ticket>(),
+                AssignedTo = new List<UserTicket>()
+            };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            var userSaved = context.Users.Find(userId);
+            var ticket = new Ticket()
+            {
+                Project = new Project(),
+                Creator = userSaved,
+                TicketId = ticketId,
+                Assigned = new List<UserTicket>()
+            };
+
+            context.Tickets.Add(ticket);
+            context.SaveChanges();
+        }
+    }
+}
